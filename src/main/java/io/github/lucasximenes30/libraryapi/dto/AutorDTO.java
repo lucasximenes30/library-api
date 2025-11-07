26db551b5f7af10add7896b97636842f0747cce7
lucasximenes30 <lucasximenes177@gmail.com>
2025-11-07 14:47:10 -0300

    feat: criando a rota get de autor.

--- src/main/java/io/github/lucasximenes30/libraryapi/dto/AutorDTO.java
+++ src/main/java/io/github/lucasximenes30/libraryapi/dto/AutorDTO.java
@@ -3,10 +3,13 @@ package io.github.lucasximenes30.libraryapi.dto;
 import io.github.lucasximenes30.libraryapi.model.Autor;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
-public record AutorDTO(String nome,
-                       LocalDate dataNascimento,
-                       String nacionalidade) {
+public record AutorDTO(
+        UUID id,
+        String nome,
+        LocalDate dataNascimento,
+        String nacionalidade) {
 
     public Autor mapearParaAutor(){
         Autor autor = new Autor();
