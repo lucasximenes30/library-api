453d484305db0b17ea1771eb2437128d7d0a16de
lucasximenes30 <lucasximenes177@gmail.com>
2025-11-07 19:55:40 -0300

    Feat: Pesquisa de autores por filtro.

--- src/main/java/io/github/lucasximenes30/libraryapi/repository/AutorRepository.java
+++ src/main/java/io/github/lucasximenes30/libraryapi/repository/AutorRepository.java
@@ -3,8 +3,13 @@ package io.github.lucasximenes30.libraryapi.repository;
 import io.github.lucasximenes30.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
+
+    List<Autor> findByNome(String nome);
+    List<Autor> findByNacionalidade(String nacionalidade);
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
 }
