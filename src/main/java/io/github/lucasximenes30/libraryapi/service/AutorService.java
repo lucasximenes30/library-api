ce4b19bc813e177aff52853318269278ea45dcb1
lucasximenes30 <lucasximenes177@gmail.com>
2026-01-28 10:07:45 -0300

    feat: Melhorando a pesquisa de autores com Query By Example

--- src/main/java/io/github/lucasximenes30/libraryapi/service/AutorService.java
+++ src/main/java/io/github/lucasximenes30/libraryapi/service/AutorService.java
@@ -6,6 +6,8 @@ import io.github.lucasximenes30.libraryapi.repository.AutorRepository;
 import io.github.lucasximenes30.libraryapi.repository.LivroRepository;
 import io.github.lucasximenes30.libraryapi.validator.AutorValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -59,6 +61,21 @@ public class AutorService {
         }
 
         return repository.findAll();
+    }
+
+    public List<Autor> pesquisaByExample( String nome, String nacionalidade) {
+        var autor = new Autor();
+        autor.setNome(nome);
+        autor.setNacionalidade(nacionalidade);
+
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreNullValues()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example<Autor> autorExample = Example.of(autor, matcher);
+
+        return repository.findAll(autorExample);
     }
 
     public boolean possuiLivro(Autor autor){
