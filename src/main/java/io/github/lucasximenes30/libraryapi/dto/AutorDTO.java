f9c57b2ded575b75b93b80ce1142dd0715aa9991
lucasximenes30 <lucasximenes177@gmail.com>
2026-01-28 09:53:11 -0300

    feat: adicionando mais funcionalidades do Bean Validation

--- src/main/java/io/github/lucasximenes30/libraryapi/dto/AutorDTO.java
+++ src/main/java/io/github/lucasximenes30/libraryapi/dto/AutorDTO.java
@@ -3,6 +3,8 @@ package io.github.lucasximenes30.libraryapi.dto;
 import io.github.lucasximenes30.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -10,10 +12,13 @@ import java.util.UUID;
 public record AutorDTO(
         UUID id,
         @NotBlank(message = "Campo obrigatorio")
+        @Size(min = 2, max = 100, message = "Campo fora do padrão")
         String nome,
         @NotNull(message = "Campo obrigatorio")
+        @Past(message = "Não pode ser uma data futura")
         LocalDate dataNascimento,
         @NotBlank(message = "Campo obrigatorio")
+        @Size(max = 50, min = 2, message = "Campo fora do padrão")
         String nacionalidade) {
 
     public Autor mapearParaAutor(){
